538371f1d4c83673e918a3c5af7b66b6bb3cffb6
lufaque <95128636+lufaque@users.noreply.github.com>
2025-03-09 21:21:41 +0300

    Refactor contract call logic to include gas parameter and prepare transaction request

--- src/relayer/common/utils/callContract.ts
+++ src/relayer/common/utils/callContract.ts
@@ -1,6 +1,7 @@
 import { Hash, type PublicClient, type SimulateContractParameters, type WalletClient } from "viem";
 import { AppErrorEnum, globalConfig } from "../../../constants";
 import { AppError } from "./AppError";
+import { prepareTransactionRequest } from "viem/actions/wallet/prepareTransactionRequest";
 
 export async function callContract(
     publicClient: PublicClient,
@@ -8,10 +9,18 @@ export async function callContract(
     simulateContractParams: SimulateContractParameters,
 ): Promise<Hash | undefined> {
     try {
-        const { request } = await publicClient.simulateContract(simulateContractParams);
+        // const { request } = await publicClient.simulateContract({
+        //     ...simulateContractParams,
+        //     gas: 1_000_000n,
+        // });
 
-        const hash = await walletClient.writeContract(request);
+        const hash = await walletClient.writeContract({
+            ...simulateContractParams,
+            // @dev we use it to avoid simulation. on arbitrum sepolia it fakely says the transaction will fail regardless of the rpc
+            gas: 1_000_000n,
+        });
 
+        // @dev TODO: We need to check the status of the tx
         const { cumulativeGasUsed } = await publicClient.waitForTransactionReceipt({
             ...globalConfig.VIEM.RECEIPT,
             hash,
