ebf0c816a221d7085339dbf554bb04997481db6e
lufaque <95128636+lufaque@users.noreply.github.com>
2025-04-29 15:58:40 +0300

    Refactor environment variable handling by removing unused networkEnvKeys import and updating getEnvAddress to use getNetworkEnvKey for network name resolution

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,3 @@
 export { AppErrorEnum, appErrors } from "./appErrors";
 export { envPrefixes } from "./envPrefixes";
 export { globalConfig } from "./globalConfig";
-export { networkEnvKeys } from "./networkEnvKeys";
--- src/relayer/common/utils/getEnvVar.ts
+++ src/relayer/common/utils/getEnvVar.ts
@@ -1,6 +1,6 @@
 import process from "process";
 import { Address } from "viem";
-import { envPrefixes, networkEnvKeys } from "../../../constants";
+import { envPrefixes } from "../../../constants";
 
 import { type env } from "../../../types/env";
 import { type EnvPrefixes } from "../../../types/envPrefixes";
@@ -13,8 +13,10 @@ export function getEnvVar<K extends keyof env>(key: K): env[K] {
 }
 
 export function getEnvAddress(prefix: keyof EnvPrefixes, networkName?: string): [Address, string] {
+    const { getNetworkEnvKey } = require("@concero/contract-utils");
+
     const searchKey = networkName
-        ? `${envPrefixes[prefix]}_${networkEnvKeys[networkName]}`
+        ? `${envPrefixes[prefix]}_${getNetworkEnvKey(networkName)}`
         : envPrefixes[prefix];
     const value = getEnvVar(searchKey as keyof env) as Address;
     const friendlyName = `${prefix}(${shorten(value)})`;
