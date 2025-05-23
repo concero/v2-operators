6285303f7c2a91311ae9c15ec6eb7eb4f9d37971
lufaque <95128636+lufaque@users.noreply.github.com>
2025-05-23 16:52:25 +0300

    update getChainOperatorMinBalance condition

--- src/relayer/common/utils/getChainOperatorMinBalance.ts
+++ src/relayer/common/utils/getChainOperatorMinBalance.ts
@@ -1,8 +1,8 @@
 import { ConceroNetwork } from "@concero/contract-utils";
 import { viemClientManager } from "../managers/ViemClientManager";
 
-const DEFAULT_GAS_LIMIT = 1_000_000n;
-const SAFE_TXS_COUNT_FOR_OPERATOR_BALANCE = 10n;
+const DEFAULT_GAS_LIMIT = 2_000_000n;
+const SAFE_TXS_COUNT_FOR_OPERATOR_BALANCE = 15n;
 
 export async function getChainOperatorMinBalance(conceroNetwork: ConceroNetwork) {
     const { publicClient } = viemClientManager?.getClients(conceroNetwork);
