68462687eaa9a088d23641c409a2e2665696de16
lufaque <95128636+lufaque@users.noreply.github.com>
2025-06-02 00:46:59 +0300

    update build

--- dist/esm/constants/index.js
+++
@@ -1,4 +0,0 @@
-export { AppErrorEnum, appErrors } from "./appErrors";
-export { envPrefixes } from "./envPrefixes";
-export { eventEmitter } from "./eventEmitter";
-export { globalConfig } from "./globalConfig";
--- dist/esm/constants/reportConfig.js
+++
@@ -1,6 +0,0 @@
-export var reportConfigBitOffsets = {
-    OFFSET_REPORT_TYPE: 248n,
-    OFFSET_VERSION: 240n,
-    OFFSET_RESERVED: 160n,
-    OFFSET_REQUESTER: 0n
-};
