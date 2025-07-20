9375da0decfe7aff58805c785e70a8a2306752e8
Oleg <olegkron.cc@gmail.com>
2025-07-20 21:58:19 +0100

    upd

--- src/common/managers/index.ts
+++ src/common/managers/index.ts
@@ -1,11 +1,7 @@
 export { initializeManagers } from "../utils/initializeManagers";
 export { BlockCheckpointManager } from "./BlockCheckpointManager";
-export { BlockManager } from "./BlockManager";
-
 export { DbManager } from "./DbManager";
-
 export { ManagerBase } from "./ManagerBase";
-
 export { TxManager } from "./TxManager";
 export { TxMonitor } from "./TxMonitor";
 export { TxReader } from "./TxReader";
