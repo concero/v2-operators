fddf408dfc32149b88e85d6a79bbf2ed79f6aa0c
d <roman@concero.io>
2025-12-04 21:54:46 +0300

    [mod]: fixed verifier executor service

--- src/relayer/relayer.ts
+++ src/relayer/relayer.ts
@@ -1,11 +1,12 @@
 import { LogModule } from './log';
-import { ManagerProvider } from './services';
+import { JobQueue, ManagerProvider } from './services';
 import { Config } from './types';
 import { VerifierModule } from './verifier';
 
 export class RelayerApp extends ManagerProvider {
     private logModule!: LogModule;
     private verifierModule!: VerifierModule;
+    private jobQueue!: JobQueue;
 
     constructor(config: Config) {
         super(config);
@@ -13,9 +14,10 @@ export class RelayerApp extends ManagerProvider {
 
     async init(): Promise<void> {
         await this.initManagers();
+        this.jobQueue = new JobQueue(this.context);
         this.logModule = new LogModule(this.context);
         await this.logModule.init();
-        this.verifierModule = new VerifierModule(this.context);
+        this.verifierModule = new VerifierModule(this.context, this.jobQueue);
         await this.verifierModule.init();
     }
 }
