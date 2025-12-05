1f31667d94ea0d9dd03a9eaf56dbaabf6c47c72f
d <roman@concero.io>
2025-12-05 17:05:55 +0300

    [mod]: removed unused code

--- src/relayer/verifier/verifer-api.service.ts
+++ src/relayer/verifier/verifer-api.service.ts
@@ -31,7 +31,7 @@ export class VerifierApiService extends ContextProvider {
         });
         this.app.get('/api/v1/processes', async (req, res) => {
             const data = await this.context.jobQueue.getAll(100);
-            res.send(JSON.stringify({ statusCode: 200, ok: true, data }));
+            res.send({ statusCode: 200, ok: true, data });
         });
         this.app.listen({ port: 5000, host: '0.0.0.0' }).catch(this.logger.error);
     }
