d0014b5683a686ca951b025f1db13102ef50430a
d <roman@concero.io>
2025-12-24 17:50:49 +0300

    [mod]: updated finality processor

--- src/relayer/validator/validator-api.service.ts
+++ src/relayer/validator/validator-api.service.ts
@@ -61,56 +61,50 @@ export class ValidatorApiService extends ContextProvider {
     }
 
     private async addConfirmationCallback(response: CRE.Response) {
-        // @todo: avoid promise.all
-        const handleResponseItem = async ([messageId, item]: [
-            messageId: string,
-            item: CRE.Response.Item,
-        ]): Promise<void> => {
-            const found = await this.context.jobQueue.findOne({ messageId });
+        await Promise.allSettled(
+            Object.entries(response || {}).map(async ([messageId, item]) => {
+                const found = await this.context.jobQueue.findOne({ messageId });
 
-            if (!found) {
-                this.logger.warn(`Job for messageId=${messageId} not found`);
-                return;
-            }
+                if (!found) {
+                    this.logger.warn(`Job for messageId=${messageId} not found`);
+                    return;
+                }
 
 
-            const foundPayload = JSON.parse(found.payload) as JobPayload;
+                const foundPayload = JSON.parse(found.payload) as JobPayload;
 
-            const mergedCallbacks: CRE.Response.Item[] = foundPayload?.callbacks
-                ? Array.from((foundPayload['callbacks'] || []) as CRE.Response.Item[]).concat(item)
-                : [item];
-            const mergedPayload = {
-                ...foundPayload,
-                callbacks: mergedCallbacks,
-            };
+                const mergedCallbacks: CRE.Response.Item[] = foundPayload?.callbacks
+                    ? Array.from((foundPayload['callbacks'] || []) as CRE.Response.Item[]).concat(item)
+                    : [item];
+                const mergedPayload = {
+                    ...foundPayload,
+                    callbacks: mergedCallbacks,
+                };
 
-            this.logger.info(`From CRE::  [messageId=${messageId}] callbacksCount=${found.callbacksCount} ${typeof found.callbacksCount} merged=${mergedCallbacks.join(';')}`)
+                this.logger.info(`From CRE::  [messageId=${messageId}] callbacksCount=${found.callbacksCount} ${typeof found.callbacksCount} merged=${mergedCallbacks.join(';')}`)
 
 
-            if (found.callbacksCount || 0 > 3) {
-                this.logger.warn(
-                    `Job messageId=${messageId} callbacks count already ${found.callbacksCount}`,
+                if (found.callbacksCount || 0 > 3) {
+                    this.logger.warn(
+                        `Job messageId=${messageId} callbacks count already ${found.callbacksCount}`,
+                    );
+                    return;
+                }
+
+
+
+                await this.context.jobQueue.updateOne(
+                    { messageId },
+                    {
+                        payload: JSON.stringify(mergedPayload),
+                        callbacksCount: mergedCallbacks.length || 0,
+                    },
                 );
-                return;
-            }
 
-
-
-            await this.context.jobQueue.updateOne(
-                { messageId },
-                {
-                    payload: JSON.stringify(mergedPayload),
-                    callbacksCount: mergedCallbacks.length || 0,
-                },
-            );
-
-            this.logger.debug(
-                `Callbacks for messageId=${messageId} count is ${mergedCallbacks?.length || 0}`,
-            );
-        };
-
-        await Promise.all(
-            Object.entries(response || {}).map(async options => handleResponseItem(options)),
+                this.logger.debug(
+                    `Callbacks for messageId=${messageId} count is ${mergedCallbacks?.length || 0}`,
+                );
+            }),
         );
     }
 }
