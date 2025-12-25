77daa4b4c5e99f047fa2ac6938170dda035df709
lufaque <95128636+lufaque@users.noreply.github.com>
2025-12-25 21:29:45 +0300

    move to postgress

--- prisma/migrations/20251020165731_add_compound_unique_relayerjob/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[jobType,txHash]` on the table `RelayerJob` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "RelayerJob_jobType_txHash_key" ON "RelayerJob"("jobType", "txHash");
