0271782ac467827a14c5d73ea4462e07d4ff95ba
romanconceroio <roman@concero.io>
2026-01-05 16:22:53 +0300

    [fix]: updated docker envs

--- src/relayer/api/api.controller.ts
+++ src/relayer/api/api.controller.ts
@@ -24,7 +24,12 @@ export class ApiController extends ContextProvider {
 
         this.app.register(
             async apiV1 => {
-                apiV1.post('/callback/cre', handleCRECallback);
+                apiV1.register(
+                    async baseApi => {
+                        baseApi.post('/callback/cre', handleCRECallback);
+                    },
+                    { prefix: '/' },
+                );
 
                 apiV1.register(
                     async managementApi => {
