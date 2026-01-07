ce551287a28358adff582c821d81e869de451adb
lufaque <95128636+lufaque@users.noreply.github.com>
2026-01-07 14:19:51 +0300

    update intervals

--- src/relayer/validator/adapters/cre-validator.adapter.ts
+++ src/relayer/validator/adapters/cre-validator.adapter.ts
@@ -99,7 +99,6 @@ export class CREValidatorAdapter extends BaseValidatorAdapter implements IValida
                         updatedAt: { lte: new Date(Date.now() - 5 * 60_000) },
                         validatorType: ValidatorType.CRE,
                         callbacksCount: { lt: 4 },
-                        // no submit retry
                     },
                 ],
             },
