2579a4e2bf84e6c38793872e2483aa92affc27e3
romanconceroio <roman@concero.io>
2026-01-13 15:14:00 +0300

    [mod]: implemented profilers, migrated to node from bun

--- docker/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-bunx prisma generate
-bunx prisma migrate deploy
-exec bun run start
--- src/relayer/services/manager.provider.ts
+++ src/relayer/services/manager.provider.ts
@@ -14,7 +14,12 @@ import { JobQueueService } from './job-queue.service';
 import { PrismaClient } from '@prisma/client';
 
 import { globalConfig } from '../../constants';
-import { DbManager, DeploymentManager, LogsListenerStore, RelayerBalanceManager, } from '../../managers';
+import {
+    DbManager,
+    DeploymentManager,
+    LogsListenerStore,
+    RelayerBalanceManager,
+} from '../../managers';
 import { Config } from '../../types';
 import { Context } from '../types';
 
