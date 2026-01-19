eb8e15a4e27cdbf5f18f418ed11156e8d6e5e1ff
romanconceroio <roman@concero.io>
2026-01-19 16:16:54 +0300

    [mod]: edited global config

--- src/constants/globalConfig.ts
+++ src/constants/globalConfig.ts
@@ -5,7 +5,7 @@ import { RelayerGlobalConfig } from '../types/GlobalConfig';
 export const globalConfig: RelayerGlobalConfig = {
     ...operatorUtilsGlobalConfig,
     chainOptionsUrl:
-        'https://github.com/concero/concero-networks/blob/476bbdd0ddf467f9b0ed78a9c45e2294cec30129/output/chains.stage.minified.json',
+        'https://raw.githubusercontent.com/concero/concero-networks/476bbdd0ddf467f9b0ed78a9c45e2294cec30129/output/chains.stage.minified.json',
     NOTIFICATIONS: {
         SLACK: {
             MONITORING_SYSTEM_CHANNEL_ID: getEnvString(
