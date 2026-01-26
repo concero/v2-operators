79b97c32ea420f8567e9245a928414021b7d2c30
lufaque <95128636+lufaque@users.noreply.github.com>
2026-01-26 16:04:35 +0300

    fix cre report signer recovery

--- src/relayer/api/api.service.ts
+++ src/relayer/api/api.service.ts
@@ -1,4 +1,4 @@
-import { concat, Hash, Hex, hexToBytes, keccak256, recoverAddress } from 'viem';
+import { encodePacked, Hash, Hex, hexToBytes, keccak256, recoverAddress } from 'viem';
 import { StandardMerkleTree } from '@openzeppelin/merkle-tree';
 import { FastifyReply, FastifyRequest } from 'fastify';
 
@@ -32,9 +32,11 @@ export class ApiService extends ContextProvider {
             ) as Hex[];
 
             const rawReportBytes = hexToBytes(rawReport);
-            const reportContextBytes = hexToBytes(reportContext);
-            const rawReportHashBytes = hexToBytes(keccak256(rawReportBytes));
-            const hashBytes = concat([rawReportHashBytes, reportContextBytes]);
+            const rawReportHashBytes = keccak256(rawReportBytes);
+            const hashBytes = encodePacked(
+                ['bytes32', 'bytes'],
+                [rawReportHashBytes, reportContext],
+            );
             const hash = keccak256(hashBytes);
 
             // validation & auth
