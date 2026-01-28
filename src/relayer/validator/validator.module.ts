2542cc26101f277bf24ffad605c116e7eb800c24
lufaque <95128636+lufaque@users.noreply.github.com>
2026-01-28 12:22:10 +0300

    update pumpPendingRequest tick

--- src/relayer/validator/validator.module.ts
+++ src/relayer/validator/validator.module.ts
@@ -38,7 +38,7 @@ export class ValidatorModule {
                     adapter.pumpPendingRequest(pumpBatchCountPerTick * creBatchSize),
                 ),
             );
-        }, 2000);
+        }, 60000);
 
         setInterval(async () => {
             await Promise.all(
