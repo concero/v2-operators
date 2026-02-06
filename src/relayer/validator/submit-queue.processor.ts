345c9aa2e03aac06ad19ae2a3c2718ca7f23732b
romanconceroio <roman@concero.io>
2026-02-06 19:33:14 +0300

    [mod]: decoupled tx submit queue from validator adapter

--- src/relayer/validator/submit-queue.processor.ts
+++ src/relayer/validator/submit-queue.processor.ts
@@ -18,13 +18,14 @@ export class SubmitQueueProcessor extends BaseValidatorService {
         if (this.isProcessing) {
             return;
         }
-
-        this.isProcessing = true;
-
-        // @todo: add custom profiling decorator
         const startTimestamp = Date.now();
 
-        const jobs = await this.context.dbClient.$queryRaw<Job[]>`
+        try {
+            this.isProcessing = true;
+
+            // @todo: add custom profiling decorator
+
+            const jobs = await this.context.dbClient.$queryRaw<Job[]>`
 WITH ranked AS (
     SELECT j.id,
            ROW_NUMBER() OVER (PARTITION BY j."dstChainSelector" ORDER BY j."lastSubmitAt" ASC) AS rn
@@ -43,87 +44,94 @@ FROM job j
         LIMIT ${options.maxTxPerPump}
 ) capped ON j.id = capped.id;
 `;
-        this.logger.info(`pump jobs.length=${jobs.length}`);
-        if (!jobs.length) {
-            return;
-        }
-
-        // @todo: support bulkWrite in TxWriter to support batches by one chain & move to batches
-        const promises = jobs.map(async job => {
-            const jobPayload = JSON.parse(job.payload) as JobPayload;
-
-            try {
-                const creCallback = await this.context.dbClient.creCallback.findFirst({
-                    where: { messageId: job.messageId },
-                });
-                if (!creCallback) {
-                    throw new Error(`Cannot find creCallback for message with id ${job.messageId}`);
-                }
-                const creResponse = JSON.parse(creCallback.payload) as CRE.Response;
-
-                const validations = this.extractJobValidations(
-                    job.validatorType as ValidatorType,
-                    job.messageId as Hex,
-                    creResponse,
-                );
-                const validatorLibs = this.extractJobValidatorLibs(
-                    job.validatorType as ValidatorType,
-                    job.dstChainSelector,
-                );
-
-                const dst = await this.submitMessage(
-                    job.dstChainSelector,
-                    jobPayload.data.messageReceipt,
-                    validations,
-                    validatorLibs,
-                );
-
-                return { jobId: job.id, type: 'success', dst };
-            } catch (e) {
-                this.logger.error(
-                    `pump Message (id=${job.messageId}, jobId=${job.id}) submit failed: ${e}`,
-                );
-                return { jobId: job.id, type: 'failed', attempts: job.submitAttempts };
+            this.logger.info(`pump jobs.length=${jobs.length}`);
+            if (!jobs.length) {
+                return;
             }
-        });
 
-        const results = await Promise.all(promises);
-        const successResults = results.filter(i => i.type === 'success');
-        const failedResults = results.filter(i => i.type === 'failed');
+            // @todo: support bulkWrite in TxWriter to support batches by one chain & move to batches
+            const promises = jobs.map(async job => {
+                const jobPayload = JSON.parse(job.payload) as JobPayload;
 
-        await this.context.dbClient.$transaction(async client => {
-            const successPromises = successResults.map(i => {
-                return client.job.update({
-                    where: { id: i.jobId },
-                    data: {
-                        status: JobStatus.WaitingDstFinality,
-                        submitAttempts: 0,
-                        submitPlannedTo: null,
-                        lastSubmitAt: new Date(),
-                        dstBlockNumber: String(i?.dst?.blockNumber),
-                        dstTxHash: String(i?.dst?.hash),
-                    },
-                });
+                try {
+                    const creCallback = await this.context.dbClient.creCallback.findFirst({
+                        where: { messageId: job.messageId },
+                    });
+                    if (!creCallback) {
+                        throw new Error(
+                            `Cannot find creCallback for message with id ${job.messageId}`,
+                        );
+                    }
+                    const creResponse = JSON.parse(creCallback.payload) as CRE.Response;
+
+                    const validations = this.extractJobValidations(
+                        job.validatorType as ValidatorType,
+                        job.messageId as Hex,
+                        creResponse,
+                    );
+                    const validatorLibs = this.extractJobValidatorLibs(
+                        job.validatorType as ValidatorType,
+                        job.dstChainSelector,
+                    );
+
+                    const dst = await this.submitMessage(
+                        job.dstChainSelector,
+                        jobPayload.data.messageReceipt,
+                        validations,
+                        validatorLibs,
+                    );
+
+                    return { jobId: job.id, type: 'success', dst };
+                } catch (e) {
+                    this.logger.error(
+                        `pump Message (id=${job.messageId}, jobId=${job.id}) submit failed: ${e}`,
+                    );
+                    return { jobId: job.id, type: 'failed', attempts: job.submitAttempts };
+                }
             });
-            const failedPromises = failedResults.map(i => {
-                const submitPlannedTo = this.calculateNextPlannedTo((i.attempts as number) + 1);
-                this.logger.warn(
-                    `Submit failed jobId=${i.jobId}, attempt=${(i?.attempts || 0) + 1}, nextTryIn=${Math.round((submitPlannedTo.getTime() - Date.now()) / 1000)}s`,
-                );
-                return client.job.update({
-                    where: { id: i.jobId },
-                    data: {
-                        submitPlannedTo,
-                        submitAttempts: { increment: 1 },
-                    },
-                });
-            });
-            const totalPromises = successPromises.concat(failedPromises);
-            await Promise.all(totalPromises);
-        });
 
-        this.logger.info(`pump took: ${(Date.now() - startTimestamp) / 1000}s`);
-        this.isProcessing = false;
+            const results = await Promise.all(promises);
+            const successResults = results.filter(i => i.type === 'success');
+            const failedResults = results.filter(i => i.type === 'failed');
+
+            await this.context.dbClient.$transaction(async client => {
+                const successPromises = successResults.map(i => {
+                    return client.job.update({
+                        where: { id: i.jobId },
+                        data: {
+                            status: JobStatus.WaitingDstFinality,
+                            submitAttempts: 0,
+                            submitPlannedTo: null,
+                            lastSubmitAt: new Date(),
+                            dstBlockNumber: String(i?.dst?.blockNumber),
+                            dstTxHash: String(i?.dst?.hash),
+                        },
+                    });
+                });
+                const failedPromises = failedResults.map(i => {
+                    const submitPlannedTo = this.calculateNextPlannedTo((i.attempts as number) + 1);
+                    this.logger.warn(
+                        `Submit failed jobId=${i.jobId}, attempt=${(i?.attempts || 0) + 1}, nextTryIn=${Math.round((submitPlannedTo.getTime() - Date.now()) / 1000)}s`,
+                    );
+                    return client.job.update({
+                        where: { id: i.jobId },
+                        data: {
+                            submitPlannedTo,
+                            submitAttempts: { increment: 1 },
+                        },
+                    });
+                });
+                const totalPromises = successPromises.concat(failedPromises);
+                await Promise.all(totalPromises);
+            });
+
+            this.logger.info(`pump succeeded for jobIds=[${jobs.map(i => i.id).join(',')}]`);
+        } catch (e) {
+            this.logger.info(`pump failed: ${e}`);
+        } finally {
+            this.logger.info(`pump took: ${(Date.now() - startTimestamp) / 1000}s`);
+            this.isProcessing = false;
+        }
     }
 
     private async submitMessage(
