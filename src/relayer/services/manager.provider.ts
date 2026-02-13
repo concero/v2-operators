96c7fe53e97c5d62b2c1e978bd387d890721178f
romanconceroio <roman@concero.io>
2026-02-13 14:53:01 +0300

    [mod]: fixed active networks

--- src/relayer/services/manager.provider.ts
+++ src/relayer/services/manager.provider.ts
@@ -81,7 +81,6 @@ export abstract class ManagerProvider {
             this.httpClient,
             60 * 1000 * 60,
         );
-        await this.chainsManager.initialize();
 
         this.dbClient = DbManager.getClient();
         await this.dbClient.$connect();
@@ -112,6 +111,7 @@ export abstract class ManagerProvider {
             this.viemClientManager,
         );
 
+        await this.chainsManager.initialize();
         await this.rpcManager.initialize();
         await this.viemClientManager.initialize();
         await this.blockManagerRegistry.initialize();
