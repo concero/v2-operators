e989dc667151bf1002b298f238174fab79b31f75
romanconceroio <roman@concero.io>
2026-03-04 16:25:58 +0300

    [mod]: fixed submit queue processor

--- src/types/job.ts
+++ src/types/job.ts
@@ -7,6 +7,7 @@ export enum JobStatus {
     PendingVerification = 'pending_verification', // verification planned, pending request
     FailedVerification = 'failed_verification', // verification failed, should be retried
     PendingSubmit = 'pending_submit', // tx submit planned, pending request
+    ProcessingSubmit = 'processing_submit', // tx submit is processing (waiting for receipt)
     WaitingDstFinality = 'waiting_dst_finality', // wait for finalization proof on dst
     Success = 'success', // tx executed and on dst side
     Failed = 'failed', // failed by reason JobErrorCode
